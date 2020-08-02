108d67908abdf150d9156607efa696bfaa9b789e
Victor <victorhugoTI@outlook.com>
2020-08-02 10:30:59 -0300

    Criando template do frontend

--- crud/frontend/src/App.vue
+++ crud/frontend/src/App.vue
@@ -1,9 +1,20 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style>
+* {
+  padding: 0px;
+  margin: 0px;
+  box-sizing: border-box;
+}
 
+:root {
+  --primary-color: #116592;
+  --bg-color: #ddd;
+  --dark-color: #222;
+  --white-color: #fff;
+}
 </style>
--- crud/frontend/src/views/Home.vue
+++ crud/frontend/src/views/Home.vue
@@ -1,18 +1,43 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header class="header"></header>
+    <aside class="aside"></aside>
+    <main class="content"></main>
+    <footer class="footer"></footer>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+};
 </script>
+
+<style>
+.home {
+  height: 100vh;
+  display: grid;
+  grid-template-areas: "aside header" "aside content" "aside footer";
+  grid-template-rows: 50px 1fr 60px;
+  grid-template-columns: 230px 1fr ;
+}
+
+.header {
+  grid-area: header;
+  background-color: var(--primary-color);
+}
+
+.aside {
+  grid-area: aside;
+  background-color: var(--dark-color);
+}
+
+.content {
+  grid-area: content;
+  background-color: var(--bg-color);
+}
+.footer {
+  grid-area: footer;
+  background-color: var(--primary-color);
+}
+</style>
